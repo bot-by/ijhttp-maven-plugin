637450654b42b1ae3189a3bb005107057ce93c9c
Witalij Berdinskich <radio_rogal@keemail.me>
2023-04-20 22:37:34 +0300

    Add a unit-test

--- src/main/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojo.java
+++ src/main/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.bot_by.maven_plugin.ijhttp;
+package uk.bot_by.maven_plugin.ijhttp_maven_plugin;
 
 import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
