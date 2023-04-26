27c58580259fbdcbf5e39f5bdc20873052ade7ff
Witalij Berdinskich <radio_rogal@posteo.pl>
2023-04-26 11:02:43 +0300

    Executor, all parameters and some unit-tests

--- src/main/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojo.java
+++ src/main/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojo.java
@@ -15,13 +15,21 @@
  */
 package uk.bot_by.maven_plugin.ijhttp_maven_plugin;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.jetbrains.annotations.VisibleForTesting;
 
 /**
  * Run integration tests using IntelliJ HTTP Client.
@@ -29,15 +37,157 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
  * @author Witalij Berdinskich
  * @see <a href="https://www.jetbrains.com/help/idea/http-client-cli.html">HTTP Client CLI</a>
  */
-@Mojo(name = "integration-test", requiresDependencyResolution = ResolutionScope.TEST, defaultPhase = LifecyclePhase.INTEGRATION_TEST, threadSafe = true)
+@Mojo(name = "integration-test", requiresDependencyResolution = ResolutionScope.TEST, defaultPhase = LifecyclePhase.INTEGRATION_TEST)
 public class IntegrationTestMojo extends AbstractMojo {
 
-  @Parameter(defaultValue = "${project.build.directory}/ijhttp-reports")
-  private File reportsDirectory;
+  private static final String CONNECT_TIMEOUT = "--connect-timeout";
+  private static final String DOCKER_MODE = "--docker-mode";
+  private static final String ENV_FILE = "--env-file";
+  private static final String ENVIRONMENT_NAME = "--env";
+  private static final String ENV_VARIABLES = "--env-variables";
+  private static final String EXECUTABLE = "ijhttp";
+  private static final String INSECURE = "--insecure";
+  private static final String LOG_LEVEL = "--log-level";
+  private static final String PRIVATE_ENV_FILE = "--private-env-file";
+  private static final String PRIVATE_ENV_VARIABLES = "--private-env-variables";
+  private static final String REPORT = "--report";
+  private static final String SOCKET_TIMEOUT = "--socket-timeout";
+
+  public enum LogLevel {BASIC, HEADERS, VERBOSE}
+
+  @Parameter(property = "ijhttp.connect-timeout")
+  private Integer connectTimeout;
+
+  @Parameter(property = "ijhttp.docker-mode")
+  private boolean dockerMode;
+
+  @Parameter(property = "ijhttp.env-file")
+  private File environmentFile;
+
+  @Parameter(property = "ijhttp.env-variables")
+  private List<String> environmentVariables;
+
+  @Parameter(property = "ijhttp.env")
+  private String environmentName;
+
+  @Parameter(property = "ijhttp.files", required = true)
+  private List<File> files;
+
+  @Parameter(property = "ijhttp.insecure", defaultValue = "false")
+  private boolean insecure;
+
+  @Parameter(property = "ijhttp.log-level", defaultValue = "BASIC")
+  private LogLevel logLevel;
+
+  @Parameter(property = "ijhttp.private-env-file")
+  private File privateEnvironmentFile;
+
+  @Parameter(property = "ijhttp.private-env-variables")
+  private List<String> privateEnvironmentVariables;
+
+  @Parameter(property = "ijhttp.report", defaultValue = "false")
+  private boolean report;
+
+  @Parameter(property = "ijhttp.skip", defaultValue = "false")
+  private boolean skip;
+
+  @Parameter(property = "ijhttp.socket-timeout")
+  private Integer socketTimeout;
+
+  public IntegrationTestMojo() {
+  }
+
+  @VisibleForTesting
+  IntegrationTestMojo(Integer connectTimeout, boolean dockerMode, File environmentFile,
+      List<String> environmentVariables, String environmentName, List<File> files, boolean insecure,
+      LogLevel logLevel, File privateEnvironmentFile, List<String> privateEnvironmentVariables,
+      boolean report, boolean skip, Integer socketTimeout) {
+    this.connectTimeout = connectTimeout;
+    this.dockerMode = dockerMode;
+    this.environmentFile = environmentFile;
+    this.environmentVariables = environmentVariables;
+    this.environmentName = environmentName;
+    this.files = files;
+    this.insecure = insecure;
+    this.logLevel = logLevel;
+    this.privateEnvironmentFile = privateEnvironmentFile;
+    this.privateEnvironmentVariables = privateEnvironmentVariables;
+    this.report = report;
+    this.skip = skip;
+    this.socketTimeout = socketTimeout;
+  }
 
   @Override
   public void execute() throws MojoExecutionException {
-    getLog().info("integration test");
+    if (skip) {
+      getLog().info("skipping execute as per configuration");
+      return;
+    }
+
+    var executor = new DefaultExecutor();
+
+    try {
+      var commandLine = getCommandLine();
+
+      executor.execute(commandLine);
+    } catch (IOException exception) {
+      throw new MojoExecutionException("I/O Error", exception);
+    }
+  }
+
+  @VisibleForTesting
+  CommandLine getCommandLine() throws IOException {
+    var commandLine = new CommandLine(EXECUTABLE);
+
+    if (nonNull(connectTimeout)) {
+      commandLine.addArgument(CONNECT_TIMEOUT).addArgument(connectTimeout.toString());
+    }
+    if (dockerMode) {
+      commandLine.addArgument(DOCKER_MODE);
+    }
+    if (nonNull(environmentFile)) {
+      commandLine.addArgument(ENV_FILE).addArgument(environmentFile.getCanonicalPath());
+    }
+    if (nonNull(environmentName) && !environmentName.isBlank()) {
+      commandLine.addArgument(ENVIRONMENT_NAME).addArgument(environmentName);
+    }
+    if (nonNull(environmentVariables)) {
+      environmentVariables.forEach(
+          variable -> commandLine.addArgument(ENV_VARIABLES).addArgument(variable));
+    }
+    if (insecure) {
+      commandLine.addArgument(INSECURE);
+    }
+    switch (logLevel) {
+      case HEADERS:
+      case VERBOSE:
+        commandLine.addArgument(LOG_LEVEL).addArgument(logLevel.name());
+      case BASIC:
+      default:
+        // do nothing
+    }
+    if (nonNull(privateEnvironmentFile)) {
+      commandLine.addArgument(PRIVATE_ENV_FILE)
+          .addArgument(privateEnvironmentFile.getCanonicalPath());
+    }
+    if (nonNull(privateEnvironmentVariables)) {
+      privateEnvironmentVariables.forEach(
+          variable -> commandLine.addArgument(PRIVATE_ENV_VARIABLES).addArgument(variable));
+    }
+    if (report) {
+      commandLine.addArgument(REPORT);
+    }
+    if (nonNull(socketTimeout)) {
+      commandLine.addArgument(SOCKET_TIMEOUT).addArgument(socketTimeout.toString());
+    }
+    if (isNull(files)) {
+      throw new IllegalArgumentException("files are required");
+    }
+    for (File file : files) {
+      commandLine.addArgument(file.getCanonicalPath());
+    }
+
+    return commandLine;
   }
 
 }
--- src/test/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojoTest.java
+++ src/test/java/uk/bot_by/maven_plugin/ijhttp_maven_plugin/IntegrationTestMojoTest.java
@@ -1,16 +1,35 @@
 package uk.bot_by.maven_plugin.ijhttp_maven_plugin;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.ArrayMatching.arrayContaining;
+import static org.hamcrest.collection.IsArrayWithSize.arrayWithSize;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.bot_by.maven_plugin.ijhttp_maven_plugin.IntegrationTestMojo.LogLevel;
 
 @ExtendWith(MockitoExtension.class)
 @Tag("fast")
@@ -18,16 +37,173 @@ class IntegrationTestMojoTest {
 
   @Mock
   private Log logger;
-  @InjectMocks
-  private IntegrationTestMojo mojo;
 
+  @DisplayName("Skip execution")
   @Test
-  void test() throws MojoExecutionException {
+  void skip() throws MojoExecutionException {
+    // given
+    var mojo = new IntegrationTestMojo(null, false, null, null, null, null, false, null, null, null,
+        false, true, null);
+
+    mojo = spy(mojo);
+    when(mojo.getLog()).thenReturn(logger);
+
     // when
     mojo.execute();
 
     // then
     verify(logger).info(anyString());
+  }
+
+  @DisplayName("Files are required")
+  @Test
+  void filesAreRequired() {
+    // given
+    var mojo = new IntegrationTestMojo(null, false, null, null, null, null, false, LogLevel.BASIC,
+        null, null, false, false, null);
+
+    // when
+    var exception = assertThrows(IllegalArgumentException.class, mojo::execute);
+
+    // then
+    assertEquals("files are required", exception.getMessage());
+  }
+
+  @DisplayName("Simple run without arguments")
+  @Test
+  void simpleRun() throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(null, false, null, null, null,
+        Collections.singletonList(file), false, LogLevel.BASIC, null, null, false, false, null);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    assertAll("File argument", () -> assertThat("how much arguments", arguments, arrayWithSize(1)),
+        () -> assertEquals("*", arguments[0], "filename"));
+  }
+
+  @DisplayName("Environment name")
+  @ParameterizedTest
+  @NullAndEmptySource
+  @ValueSource(strings = {"   ", "envname"})
+  void environmentName(String name) throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(null, false, null, null, name,
+        Collections.singletonList(file), false, LogLevel.BASIC, null, null, false, false, null);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    if (isNull(name) || name.isBlank()) {
+      assertAll("Environment name is not set",
+          () -> assertThat("how much arguments", arguments, arrayWithSize(1)),
+          () -> assertEquals("*", arguments[0], "filename"));
+    } else {
+      assertThat("environment name", arguments, arrayContaining("--env", name, "*"));
+    }
+  }
+
+  @DisplayName("Quoted environment name")
+  @Test
+  void quotedEnvironmentName() throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(null, false, null, null, "environment name",
+        Collections.singletonList(file), false, LogLevel.BASIC, null, null, false, false, null);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    assertThat("environment name", arguments,
+        arrayContaining("--env", "\"environment name\"", "*"));
+  }
+
+  @DisplayName("Logger levels")
+  @ParameterizedTest
+  @CsvSource(value = {"BASIC, 1, N/A", "HEADERS, 3, HEADERS",
+      "VERBOSE, 3, VERBOSE"}, nullValues = "N/A")
+  void loggerLevels(LogLevel logLevel, int howMuchArguments, String logLevelName)
+      throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(null, false, null, null, null,
+        Collections.singletonList(file), false, logLevel, null, null, false, false, null);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    assertThat("how much arguments", arguments, arrayWithSize(howMuchArguments));
+    if (nonNull(logLevelName)) {
+      assertEquals(logLevelName, arguments[howMuchArguments - 2], "name of logger level");
+    }
+  }
+
+  @DisplayName("Timeouts")
+  @ParameterizedTest
+  @CsvSource(value = {"connect timeout, 345, N/A, 345, --connect-timeout",
+      "socket timeout, N/A, 765, 765, --socket-timeout"}, nullValues = "N/A")
+  void timeouts(String testName, Integer connectTimeout, Integer socketTimeout,
+      String argumentValue, String argumentName) throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(connectTimeout, false, null, null, null,
+        Collections.singletonList(file), false, LogLevel.BASIC, null, null, false, false,
+        socketTimeout);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    assertThat(testName, arguments, arrayContaining(argumentName, argumentValue, "*"));
+  }
+
+  @DisplayName("Flag arguments")
+  @ParameterizedTest
+  @CsvSource({"docker mode, true, false, false, --docker-mode",
+      "insecure, false, true, false, --insecure", "report, false, false, true, --report"})
+  void flagArguments(String testName, boolean dockerMode, boolean insecure, boolean report,
+      String argumentName) throws IOException {
+    // given
+    var file = mock(File.class);
+    var mojo = new IntegrationTestMojo(null, dockerMode, null, null, null,
+        Collections.singletonList(file), insecure, LogLevel.BASIC, null, null, report, false, null);
+
+    when(file.getCanonicalPath()).thenReturn("*");
+
+    // when
+    var commandLine = mojo.getCommandLine();
+
+    // then
+    var arguments = commandLine.getArguments();
+
+    assertThat(testName, arguments, arrayContaining(argumentName, "*"));
   }
 
 }
